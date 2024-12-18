2b89ef560a467d2487c64d9a66f43ae7ecb53ab1
Schaharyar <schaharyar@gmail.com>
2024-12-18 21:49:39 +0500

    ui changes

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,113 +1,125 @@
 'use client'
-import React, {useState} from 'react'
-import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/16/solid'
-import emailjs from '@emailjs/browser'
-import { toast } from 'react-toastify'
+import React, { useState } from 'react';
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid';
+import emailjs from '@emailjs/browser';
+import { toast } from 'react-toastify';
 
-
-interface Props {
-    
-}
+interface Props {}
 
 const ContactMe: React.FC<Props> = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
+    const serviceId = 'service_eq4a25d';
+    const templateId = 'template_n9q3m2m';
+    const publickey = 'pfX1zOv3KaGSPj6Pl';
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Schaheryar',
+      message: message,
+    };
 
+    emailjs
+      .send(serviceId, templateId, templateParams, publickey)
+      .then(() => {
+        toast.success('Form Submitted!');
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        toast.error('Something went wrong!');
+        console.error('Error sending email:', error);
+      });
+  };
 
-        // Emailjs services
-        const serviceId = 'service_eq4a25d';
-        const templateId = 'template_n9q3m2m';
-        const publickey = 'pfX1zOv3KaGSPj6Pl';
+  return (
+    <div
+      className="relative flex flex-col justify-center items-center text-left z-0 px-6 py-8 md:px-10"
+      style={{
+        backgroundImage: `url('/bg-footer.jpg')`, // Replace with your image path
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh', // Increased height of the section
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50 z-0" />
 
-        const templateParams = {
-            from_name: name,
-            from_email: email,
-            to_name: 'Schaheryar',
-            message: message
-        };
+      {/* Centered Heading */}
+      <h3 className="absolute top-24 w-full text-center uppercase tracking-[20px] text-gray-500 text-3xl">
+        Contact
+      </h3>
 
-        // sending the mail
-        emailjs.send(serviceId, templateId, templateParams, publickey)
-        .then((Response) => {
-            // alert('Message Sent Successfully')
-            toast.success("Form Submitted!")
-            // console.log('Email send Successfully!', Response )
-            setName('');
-            setEmail('');
-            setMessage('');
-        })
-        .catch((error) => {
-            toast.error('Something went wrong!')
-            console.log('Error sending enail:', error)
-        })
+      {/* Content Wrapper */}
+      <div className="relative z-10 flex flex-col md:flex-row w-full max-w-screen-lg mx-auto space-y-6 md:space-y-0 md:space-x-12 items-start">
+        {/* Text Section */}
+        <div className="flex flex-col space-y-4 w-full md:w-1/2">
+          <h4 className="text-2xl font-semibold text-gray-100">
+            I have just what you need.{' '}
+            <span className="underline decoration-[#F7AB0A]/50">Let's Talk</span>
+          </h4>
 
-    }
-
-    return (
-        <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row
-        max-w-ful justify-evenly mx-auto items-center z-0'>
-             <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Contact</h3> 
-
-            <div className='flex flex-col space-y-5'>
-                <h4 className='text-2xl md:text-4xl font-semibold text-center'>
-                    I have got just what you need.{""}
-                    <span className='decoration-[#F7AB0A]/50 underline'>Lets Talk</span>
-                </h4>
-
-                <div className='space-y-5'>
-                    <div className='flex items-center space-x-5 justify-center'>
-                    <PhoneIcon className='text-[#F7AB0A] h- w-7 animate-pulse'/>
-                    <p className='text-xl md:text-2xl'>+923049347176</p>
-                    </div>
-
-                    <div className='flex items-center space-x-5 justify-center'>
-                    <EnvelopeIcon className='text-[#F7AB0A] h- w-7 animate-pulse'/>
-                    <p className='text-xl md:text-2xl'>xheharyartariq@gmail.com</p>
-                    </div>
-
-                    <div className='flex items-center space-x-5 justify-center'>
-                    <MapPinIcon className='text-[#F7AB0A] h- w-7 animate-pulse'/>
-                    <p className='text-xl md:text-2xl'>123 Developer Lane</p>
-                    </div>
-                </div>
-
-                <form onSubmit={handleSubmit}
-                    className='flex flex-col space-y-2 w-[350px] md:w-[400px] mx-auto'>
-                    <input  
-                    className='contactInput' 
-                    placeholder='Name' 
-                    type="text"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)} 
-                    required/>
-                    <input  
-                    className='contactInput' 
-                    placeholder='Email' 
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)} 
-                    required/>
-                    <textarea  
-                    className='contactInput' 
-                    placeholder='Message'
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)} 
-                    required/>
-                    <button
-                    type='submit'  
-                    className='bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg'>Submit</button>
-                </form>
-
-
+          <div className="space-y-3 text-gray-200">
+            <div className="flex items-center space-x-3">
+              <PhoneIcon className="text-[#F7AB0A] h-5 w-5 animate-pulse" />
+              <p className="text-lg">+92-304-9347176</p>
             </div>
+            <div className="flex items-center space-x-3">
+              <EnvelopeIcon className="text-[#F7AB0A] h-5 w-5 animate-pulse" />
+              <p className="text-lg">schaharyar@gmail.com</p>
+            </div>
+            <div className="flex items-center space-x-3">
+              <MapPinIcon className="text-[#F7AB0A] h-5 w-5 animate-pulse" />
+              <p className="text-lg">123 Developer Lane</p>
+            </div>
+          </div>
         </div>
-    )
-}
 
-export default ContactMe
+        {/* Form Section */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col space-y-3 w-full md:w-1/2"
+        >
+          <input
+            className="contactInput"
+            placeholder="Name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            className="contactInput"
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <textarea
+            className="contactInput"
+            placeholder="Message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-3 px-6 rounded-md text-black font-bold text-base"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -36,26 +36,21 @@ const Projects: React.FC<Props> = () => {
         },
     ];
 
-    // State for selected category and current project index
-    const [selectedCategory, setSelectedCategory] = useState("All");
-    const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    // Filtered projects based on category
-    const filteredProjects =
-        selectedCategory === "All"
-            ? projects
-            : projects.filter((project) => project.category === selectedCategory);
-
-    // Handle next/previous project navigation
-    const handleNextProject = () => {
-        setCurrentProjectIndex((prev) => (prev + 1) % filteredProjects.length);
+    // Handle navigation arrows
+    const handleNext = () => {
+        setCurrentIndex((prev) => (prev + 2) % projects.length);
     };
 
-    const handlePrevProject = () => {
-        setCurrentProjectIndex((prev) =>
-            prev === 0 ? filteredProjects.length - 1 : prev - 1
+    const handlePrev = () => {
+        setCurrentIndex((prev) =>
+            prev === 0 ? projects.length - 2 : (prev - 2 + projects.length) % projects.length
         );
     };
+
+    // Calculate the currently visible projects
+    const visibleProjects = projects.slice(currentIndex, currentIndex + 2);
 
     return (
         <div className="relative h-screen flex flex-col items-center justify-center max-w-full px-6 mx-auto overflow-hidden z-0">
@@ -64,135 +59,76 @@ const Projects: React.FC<Props> = () => {
                 Projects
             </h3>
 
-            {/* Filter Buttons */}
-            <div className="flex gap-4  md:mt-[15rem] sm:mt-0">
-                {["All", "Tools", "Web App", "Full Stack"].map((category) => (
-                    <button
-                        key={category}
-                        onClick={() => setSelectedCategory(category)}
-                        className={`px-4 py-2 rounded-lg text-sm font-semibold ${
-                            selectedCategory === category
-                                ? "bg-[#F7AB0A] text-white"
-                                : "bg-gray-200 text-gray-800"
-                        } transition duration-300 hover:bg-[#F7AB0A] hover:text-white`}
-                    >
-                        {category}
-                    </button>
-                ))}
-            </div>
-
-            {/* Mobile Navigation Arrows */}
-            <div className="relative w-full max-w-5xl p-10 mt-2 mx-auto flex justify-center items-center md:hidden">
+            {/* Project Cards */}
+            <div className="relative w-full max-w-5xl mt-20 flex justify-center items-center">
                 {/* Arrow Left */}
                 <button
-                    onClick={handlePrevProject}
-                    className="absolute left-0 z-10 bg-gray-800 text-white p-6 md:p-8 rounded-full shadow-lg animate-pulse hover:bg-gray-700 hover:scale-110 transition duration-300"
+                    onClick={handlePrev}
+                    className="absolute left-4 z-10 bg-gray-800 text-white p-4 md:p-6 rounded-full shadow-lg animate-pulse hover:bg-gray-700 hover:scale-110 transition duration-300"
                 >
                     &#8592;
                 </button>
 
-                {/* Project Card (display one project at a time on mobile) */}
+                {/* Project Container */}
                 <motion.div
-                    key={filteredProjects[currentProjectIndex].title}
-                    className="relative group overflow-hidden rounded-lg shadow-md cursor-pointer w-full"
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
+                    className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-4xl px-4"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
                     transition={{ duration: 0.6 }}
-                    viewport={{ once: true }}
                 >
-                    {/* Project Image */}
-                    <motion.img
-                        src={filteredProjects[currentProjectIndex].image}
-                        alt={filteredProjects[currentProjectIndex].title}
-                        className="object-contain w-full h-64 transition-transform duration-500 group-hover:scale-105"
-                    />
-
-                    {/* Overlay */}
-                    <div
-                        className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-500"
-                        role="presentation"
-                    ></div>
-
-                    {/* Content */}
-                    <div className="absolute bottom-0 left-0 right-0 p-4 text-white transform translate-y-full group-hover:translate-y-0 transition-transform duration-500">
-                        <h4 className="text-xl font-bold group-hover:text-[#F7AB0A] transition-colors duration-300">
-                            {filteredProjects[currentProjectIndex].title}
-                        </h4>
-                        <p className="text-sm mt-1">
-                            {filteredProjects[currentProjectIndex].description}
-                        </p>
-                        <a
-                            href={filteredProjects[currentProjectIndex].link}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="inline-flex items-center mt-2 text-[#F7AB0A] hover:underline"
+                    {visibleProjects.map((project, index) => (
+                        <motion.div
+                            key={project.title}
+                            className="relative group overflow-hidden rounded-lg shadow-md cursor-pointer"
+                            initial={{ opacity: 0, y: 50 }}
+                            whileInView={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.6, delay: index * 0.2 }}
+                            viewport={{ once: true }}
                         >
-                            View Project
-                            <img
-                                src="/external-link.png"
-                                alt="External Link"
-                                className="h-4 w-4 ml-2"
+                            {/* Project Image */}
+                            <motion.img
+                                src={project.image}
+                                alt={project.title}
+                                className="object-cover w-full h-64 transition-transform duration-500 group-hover:scale-105"
                             />
-                        </a>
-                    </div>
+
+                            {/* Overlay */}
+                            <div
+                                className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-500"
+                                role="presentation"
+                            ></div>
+
+                            {/* Content */}
+                            <div className="absolute bottom-0 left-0 right-0 p-4 text-white transform translate-y-full group-hover:translate-y-0 transition-transform duration-500">
+                                <h4 className="text-xl font-bold group-hover:text-[#F7AB0A] transition-colors duration-300">
+                                    {project.title}
+                                </h4>
+                                <p className="text-sm mt-1">{project.description}</p>
+                                <a
+                                    href={project.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="inline-flex items-center mt-2 text-[#F7AB0A] hover:underline"
+                                >
+                                    View Project
+                                    <img
+                                        src="/external-link.png"
+                                        alt="External Link"
+                                        className="h-4 w-4 ml-2"
+                                    />
+                                </a>
+                            </div>
+                        </motion.div>
+                    ))}
                 </motion.div>
 
                 {/* Arrow Right */}
                 <button
-                    onClick={handleNextProject}
-                    className="absolute right-0 z-10 bg-gray-800 text-white p-6 md:p-8 rounded-full shadow-lg animate-pulse hover:bg-gray-700 hover:scale-110 transition duration-300"
+                    onClick={handleNext}
+                    className="absolute right-4 z-10 bg-gray-800 text-white p-4 md:p-6 rounded-full shadow-lg animate-pulse hover:bg-gray-700 hover:scale-110 transition duration-300"
                 >
                     &#8594;
                 </button>
-            </div>
-
-            {/* Project Grid */}
-            {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full p-10 mt-2"> */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 w-full max-w-5xl p-10 mt-2 mx-auto hidden md:grid place-items-center">
-                {filteredProjects.map((project, index) => (
-                    <motion.div
-                        key={index}
-                        className="relative group overflow-hidden rounded-lg shadow-md cursor-pointer"
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.6, delay: index * 0.2 }}
-                        viewport={{ once: true }}
-                    >
-                        {/* Project Image */}
-                        <motion.img
-                            src={project.image}
-                            alt={project.title}
-                            className="object-cover w-full h-64 transition-transform duration-500 group-hover:scale-105"
-                        />
-
-                        {/* Overlay */}
-                        <div
-                            className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-500"
-                            role="presentation"
-                        ></div>
-
-                        {/* Content */}
-                        <div className="absolute bottom-0 left-0 right-0 p-4 text-white transform translate-y-full group-hover:translate-y-0 transition-transform duration-500">
-                            <h4 className="text-xl font-bold group-hover:text-[#F7AB0A] transition-colors duration-300">
-                                {project.title}
-                            </h4>
-                            <p className="text-sm mt-1">{project.description}</p>
-                            <a
-                                href={project.link}
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="inline-flex items-center mt-2 text-[#F7AB0A] hover:underline"
-                            >
-                                View Project
-                                <img
-                                    src="/external-link.png"
-                                    alt="External Link"
-                                    className="h-4 w-4 ml-2"
-                                />
-                            </a>
-                        </div>
-                    </motion.div>
-                ))}
             </div>
 
             {/* Background Decoration */}
